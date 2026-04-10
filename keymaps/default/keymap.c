cd1b4686356456772afc919dad6ec2d87c5a0aa5
Bravo <98163699+BravoHotshot@users.noreply.github.com>
2026-04-10 21:19:56 +0530

    Updated keymap

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -27,15 +27,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_NAV] = LAYOUT_split_3x5_3(
   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,        KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-  KC_F11,  KC_F12,  KC_UP,   KC_PGUP, KC_PGDN,      KC_END,  KC_HOME, XXXXXXX, XXXXXXX, XXXXXXX,
-  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, KC_TAB,       XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-                    _______, KC_DEL,  _______,      _______, NAV_ADJ, _______
+  KC_F11,  KC_F12,  KC_UP,   XXXXXXX, XXXXXXX,      XXXXXXX, KC_PGUP, KC_HOME, XXXXXXX, XXXXXXX,
+  XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT, XXXXXXX,      XXXXXXX, KC_PGDN, KC_END,  XXXXXXX, XXXXXXX,
+                    _______, _______, KC_DEL,       _______, NAV_ADJ, _______
 ),
 
 [_NUM] = LAYOUT_split_3x5_3(
   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
-  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,
-  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,      KC_PIPE, KC_LCBR, KC_RCBR, KC_UNDS, KC_PLUS,
+  KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                     _______, KC_GRV,  KC_QUOT,      _______, _______, _______
 ),
 
